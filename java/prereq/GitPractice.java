7c9b5c59da2a3839b045ea90c6ba63fdf62dc334
Quy Duc <s4070049@rmit.edu.vn>
2024-05-10 08:54:26 +0700

    Change Mem1

--- java/prereq/GitPractice.java
+++ java/prereq/GitPractice.java
@@ -18,7 +18,8 @@ public class GitPractice {
 
    public static void groupMember1() {
       // TODO: Group Member 1, place your code here
-      System.out.println();
+      System.out.println("Hi");
+      
    }
 
    public static void groupMember2() {
