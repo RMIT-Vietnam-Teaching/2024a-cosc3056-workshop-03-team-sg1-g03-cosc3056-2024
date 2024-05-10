c1fa101bab3be3d9dd63c861bcd5d2ab1b94e08f
Tran Huu Viet Hung <s3975170@rmit.edu.vn>
2024-05-10 09:13:48 +0700

    change gm1

--- java/prereq/GitPractice.java
+++ java/prereq/GitPractice.java
@@ -18,7 +18,7 @@ public class GitPractice {
 
    public static void groupMember1() {
       // TODO: Group Member 1, place your code here
-      System.out.println("Hi");
+      System.out.println("Hi all");
       
    }
 
