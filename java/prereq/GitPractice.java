f487a61149acc0f19a1220b6ede421bbd597eb07
Bao Toan <s4045102@rmit.edu.vn>
2024-05-10 08:45:27 +0700

    Changed mem3 and merge

--- java/prereq/GitPractice.java
+++ java/prereq/GitPractice.java
@@ -18,19 +18,21 @@ public class GitPractice {
 
    public static void groupMember1() {
       // TODO: Group Member 1, place your code here
+      System.out.println();
    }
 
    public static void groupMember2() {
       // TODO: Group Member 2, place your code here
+      System.out.println();
    }
 
    public static void groupMember3() {
-      // TODO: Group Member 3 (if you have a group of 3), place your code here
+      System.out.println("Hi I'm member 3");
    }
 
    public static void mergeConflictArea() {
       // TODO: All Group members uncomment and update this line of code
-      //System.out.println("CHANGE ME");
+      System.out.println("CHANGE ME");
    }
 
 }
